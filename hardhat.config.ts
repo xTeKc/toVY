8f85e264d4f05a60c401d4762537231ab6ca6fcc
xTeKc <bcd3v@pm.me>
2021-07-27 18:40:41 -0400

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import { HardhatUserConfig } from "hardhat/config";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -11,9 +12,14 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
   }
 });
 
+const config: HardhatUserConfig = {
+  // Your type-safe config goes here
+};
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 export default {
   solidity: "0.8.0",
+  config,
 };
