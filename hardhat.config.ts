a8446b58b16fb61a035b2a2da65557c1886b0424
xTeKc <bcd3v@pm.me>
2021-07-27 18:21:20 -0400

    move / delete

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,17 @@
+import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-vyper";
+import "hardhat-typechain";
 
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.address);
+  }
+});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -8,7 +19,7 @@ import "@nomiclabs/hardhat-vyper";
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
- module.exports = {
+ export default {
   vyper: {
     version: "0.1.0b10",
   },
