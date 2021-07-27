e6b5ce64b0c897ffc8f89de6f6ea0545e2f98a12
xTeKc <bcd3v@pm.me>
2021-07-27 18:33:48 -0400

    modify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,12 @@
-import { task } from "hardhat/config";
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-vyper";
-import "hardhat-typechain";
+require("@nomiclabs/hardhat-waffle");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(await account.address);
+    console.log(account.address);
   }
 });
 
@@ -19,8 +16,6 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
- export default {
-  vyper: {
-    version: "0.1.0b10",
-  },
+module.exports = {
+  solidity: "0.8.4",
 };
