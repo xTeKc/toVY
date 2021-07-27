9a23675992668abe176ee7acb32523b63bcea8b7
xTeKc <bcd3v@pm.me>
2021-07-27 18:02:33 -0400

    install vyper plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,6 @@
-require("@nomiclabs/hardhat-waffle");
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-vyper";
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
